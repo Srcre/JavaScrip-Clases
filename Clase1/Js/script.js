e3578f92acb401793c753c9a073c4d182c40ebc6
Mr-Anon <nicolashernandeznh0234@gmail.com>
2025-09-29 17:52:15 -0300

    La declaración de datos
    Se esta viendo lo más basicó de java, declaraciones, etc.

---
+++ Clase1/Js/script.js
@@ -0,0 +1,18 @@
+// Declaracion de variables
+// let
+
+// Siempre se recomienda usar let para almacenaje de datos
+//let "cualquier cosa: "
+
+
+// let x = parseInt(prompt("Ponte vio, ingresa algo lonji: "));
+// let y = parseInt(prompt("Ponte vio, ingresa otra cosa lonji: "));
+
+// alert(x + y);
+
+let hora = parseInt(prompt("Ingresa el tiempo en hora: "));
+let recorrido = parseInt(prompt("Ingresa la distancia en kilometros: "));
+
+let texto = "tu valor es: "
+
+//completar...
